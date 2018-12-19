a216ab2efef5873d076d698907079a39abaeed82
carlos <eduarbboy12@gmail.com>
2018-12-18 20:14:33 -0500

    Reestructuracion del rojecto en git

---
+++ src/patterns/BuildersDisignPattern/Demo.java
@@ -0,0 +1,11 @@
+package patterns.BuildersDisignPattern;
+
+public class Demo {
+    public static void main(String[] args) {
+        String hello = "hello";
+        System.out.println("<p>" + hello + "</p>");
+
+        String [] words = {"hello", "world"};
+        System.out.println("<p>" + hello + "</p>");
+    }
+}
